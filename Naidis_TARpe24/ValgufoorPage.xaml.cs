d3a445756b4946f00684503d3aa351d6db210922
Siim-byte <beloimiis@gmail.com>
2026-02-17 19:22:19 +0200

    Update ValgufoorPage.xaml.cs

--- Naidis_TARpe24/ValgufoorPage.xaml.cs
+++ Naidis_TARpe24/ValgufoorPage.xaml.cs
@@ -5,22 +5,23 @@ namespace Naidis_TARpe24;
 public partial class ValgufoorPage : ContentPage
 {
 	BoxView BoxView;
-	Ellipse foor;
+    Label status;
+    bool sisse = false;
+    Ellipse foor;
 	Ellipse punane;
 	Ellipse roheline;
 	Ellipse kollane;
 	List<string> nupud = new List<string>() { "Tagasi", "Avaleht", "Edasi", "Sisse", "Välja" };
 	HorizontalStackLayout hsl;
 	VerticalStackLayout vsl;
-	public ValgusFoorPage()
+	public ValgufoorPage()
 	{
-		foor = new Ellipse()
+		BoxView = new BoxView()
 		{
 			WidthRequest = 180,
 			HeightRequest = 180,
-			Fill=new SolidColorBrush(Colors.Gray),
-			StrokeThickness =5,
-			HorizontalOptions=LayoutOptions.Center
+			BackgroundColor = Colors.Black,
+            HorizontalOptions =LayoutOptions.Center
 		};
 		punane = new Ellipse()
 		{
@@ -60,19 +61,82 @@ public partial class ValgufoorPage : ContentPage
                 HeightRequest = 50,
                 ZIndex = j
             };
+            nupp.Clicked += Foorid;
             hsl.Add(nupp);
         }
         vsl = new VerticalStackLayout()
         {
             Padding = 20,
             Spacing = 15,
-            Children = { BoxView, punane, roheline, kollane, hsl},
+            Children = { BoxView, punane, roheline, kollane, hsl, status},
             HorizontalOptions = LayoutOptions.Center
         };
         Content = vsl;
-	}
+        status = new Label()
+        {
+            Text = "Vali valgus",
+            FontSize = 25,
+            HorizontalOptions = LayoutOptions.Center
+        };
+        TapGestureRecognizer tap_kollane = new TapGestureRecognizer();
+        tap_kollane.NumberOfTapsRequired = 1;
+        kollane.GestureRecognizers.Add(tap_kollane);
+        tap_kollane.Tapped += (sender, e) =>
+        {
+            if (sisse)
+            {
+                status.Text = "Valmistu";
+                punane.Fill = new SolidColorBrush(Colors.Gray);
+                kollane.Fill = new SolidColorBrush(Colors.Yellow);
+                roheline.Fill = new SolidColorBrush(Colors.Gray);
+            }
+        };
+        TapGestureRecognizer tap_roheline = new TapGestureRecognizer();
+        tap_roheline.NumberOfTapsRequired = 1;
+        roheline.GestureRecognizers.Add(tap_roheline);
+        tap_roheline.Tapped += (sender, e) =>
+        {
+            if (sisse)
+            {
+                status.Text = "Sõida";
+                punane.Fill = new SolidColorBrush(Colors.Gray);
+                kollane.Fill = new SolidColorBrush(Colors.Gray);
+                roheline.Fill = new SolidColorBrush(Colors.Green);
+            }
+        };
+        TapGestureRecognizer tap_punane = new TapGestureRecognizer();
+        tap_punane.NumberOfTapsRequired = 1;
+        punane.GestureRecognizers.Add(tap_punane);
+        tap_punane.Tapped += (sender, e) =>
+        {
+            if (sisse)
+            {
+                status.Text = "Seisa";
+                punane.Fill = new SolidColorBrush(Colors.Red);
+                kollane.Fill = new SolidColorBrush(Colors.Gray);
+                roheline.Fill = new SolidColorBrush(Colors.Gray);
+            }
+        };
+    }
 	private void Foorid(object? sender, EventArgs e)
 	{
 		Button nupp = sender as Button;
-	}
+
+        if (nupp.Text == "Sisse")
+        {
+            sisse = true;
+            status.Text = "Vali valgu";
+            punane.Fill = new SolidColorBrush(Colors.Red);
+            kollane.Fill = new SolidColorBrush(Colors.Yellow);
+            roheline.Fill = new SolidColorBrush(Colors.Green);
+        }
+        else if (nupp.Text == "Välja")
+        {
+            sisse = false;
+            status.Text = "Lülita esmalt foor sisse";
+            punane.Fill = new SolidColorBrush(Colors.Gray);
+            kollane.Fill = new SolidColorBrush(Colors.Gray);
+            roheline.Fill = new SolidColorBrush(Colors.Gray);
+        }
+    }
 }
