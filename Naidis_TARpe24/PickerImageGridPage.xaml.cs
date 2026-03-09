20c4647978fb95679c10b862e363240f8bcbcabf
opilane <opilane@HP-CZC2349HTZ>
2026-03-09 12:35:52 +0200

    started grid

--- Naidis_TARpe24/PickerImageGridPage.xaml.cs
+++ Naidis_TARpe24/PickerImageGridPage.xaml.cs
@@ -24,5 +24,64 @@ public partial class PickerImageGridPage : ContentPage
 				new ColumnDefinition {Width = new GridLength(1,GridUnitType.Star)},
 			},
 		};
+		picker = new Picker()
+		{
+			Title = "Vali pilt",
+			ItemsSource = new List<string> { "Pilt 1", "Pilt 2", "Pilt 3" },
+			HorizontalOptions = LayoutOptions.Center,
+			VerticalOptions = LayoutOptions.Center
+		};
+		picker.SelectedIndexChanged += Piltide_valik;
+		img = new Image()
+		{
+			Source = "dotnet_bot.png",
+			HorizontalOptions = LayoutOptions.Center,
+		};
+		s_pilt = new Switch
+		{
+			HorizontalOptions = LayoutOptions.Center,
+			VerticalOptions = LayoutOptions.Center,
+			IsToggled = true,
+			IsEnabled = true
+		};
+		s_pilt.Toggled += (sender, e) =>
+		{
+			if (e.Value)
+			{
+				img.IsVisible = true;
+			}
+			else
+			{
+				img.IsVisible = false;
+			}
+		};
+		s_grid = new Switch()
+		{
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+			IsToggled = false,
+			IsEnabled = true
+        };
+		s_grid.Toggled += (sender, e) =>
+		{
+			if (e.Value)
+			{
+				//gr3x3 = Täida_gr3x3();//kutsuime välja funktsiooni, mis loob 3x3 gridi ja lisab sinna kastid, mis muudavad värvi
+
+				gr4x1.Add(gr3x3, 0, 2);
+				gr4x1.SetColumnSpan(gr3x3, 2);
+			}
+			else
+			{
+				gr4x1.RemoveAt(4);
+			}
+		};
+	}
+	private void Piltide_valik(object? sender, EventArgs e)
+	{
+		if (picker.SelectedIndex == -1) return; //kui midagi pole valitud, siis ei tee midagi
+		if (picker.SelectedIndex == 0) img.Source = "pilt1.jpg";
+		else if (picker.SelectedIndex == 1) img.Source = "pilt2.jpg";
+		else if (picker.SelectedIndex == 2) img.Source = "pilt3.jpg";
 	}
 }
